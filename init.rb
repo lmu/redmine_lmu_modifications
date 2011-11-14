d187e709d7c1bf72e42c4fe16cce2ae72d0b6c93
Kirill Bezrukov <kirill.bezrukov@gmail.com>
2011-11-14 14:40:47 +0000

    git-svn-id: svn://redminecrm.com/redmine_favorite_projects/trunk@446 eca72092-3dbd-4bd6-9333-8708c660e71c

--- init.rb
+++ init.rb
@@ -1,16 +1,16 @@
 require 'redmine'
 require 'dispatcher'  
 
-require 'redmine_projects_extensions/patches/application_helper_patch'
+require 'redmine_favorite_projects/patches/application_helper_patch'
 # require 'redmine_projects_extensions/patches/projects_controller_patch'
-require 'redmine_projects_extensions/patches/projects_helper_patch'
+require 'redmine_favorite_projects/patches/projects_helper_patch'
 
 
-Redmine::Plugin.register :redmine_projects_extensions do
-  name 'Redmine Projects Extensions plugin'
-  author 'Kirill Bezrukov'
+Redmine::Plugin.register :redmine_favorite_projects do
+  name 'Redmine Favorite Projects plugin'
+  author 'RedmineCRM'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'http://redminecrm.com'
+  author_url 'mailto:kirbez@redminecrm.com'
 end
