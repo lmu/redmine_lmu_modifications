bfb1e301962b81036c9ab84d46675a4fd9252440
Kirill Bezrukov <kirill.bezrukov@gmail.com>
2011-11-14 14:33:27 +0000

    git-svn-id: svn://redminecrm.com/redmine_projects_extensions/trunk@441 eca72092-3dbd-4bd6-9333-8708c660e71c

---
+++ lib/redmine_favorite_projects/patches/projects_helper_patch.rb
@@ -0,0 +1,22 @@
+module RedmineFavoriteProjects
+  module Patches
+
+    module ProjectsHelperPatch
+
+      def self.included(base) # :nodoc:
+        base.send(:include, InstanceMethods)
+      end
+
+      module InstanceMethods
+        include FavoriteProjectsHelper
+      end
+
+    end
+  end
+end
+
+Dispatcher.to_prepare do  
+  unless ProjectsHelper.included_modules.include?(RedmineFavoriteProjects::Patches::ProjectsHelperPatch)
+    ProjectsHelper.send(:include, RedmineFavoriteProjects::Patches::ProjectsHelperPatch)
+  end
+end
