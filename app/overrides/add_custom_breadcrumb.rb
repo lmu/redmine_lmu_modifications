6ef3a4a738bb7ebd4a0c298cd4bc1e04f112160d
Alexander Meindl <a.meindl@alphanodes.com>
2017-03-09 19:50:37 +0100

    Remove breadcrumb and head title support

--- app/overrides/add_custom_breadcrumb.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new virtual_path: 'layouts/base',
-                     name:         'add-custom-breadcrumb',
-                     original:     'be430ec43dcb16de977858ed7c4fbe8471d6dbfb',
-                     insert_after: 'div#header',
-                     partial:      'base/custom_breadcrumb'
--- init.rb
+++ init.rb
@@ -1,18 +1,9 @@
-# encoding: utf-8
-#
-
-require 'redmine'
-
 Redmine::Plugin.register :redmine_lmu_modifications do
   name 'Redmine LMU modifications plugin'
   author 'AlphaNodes GmbH'
   description 'Customizing for LMU Redmine'
-  version '0.0.3'
+  version '0.0.4'
   author_url 'https://alphanodes.com'
-end
-
-unless ApplicationHelper.included_modules.include? RedmineLmuModifications::Patches::ApplicationHelperPatch
-  ApplicationHelper.send(:include, RedmineLmuModifications::Patches::ApplicationHelperPatch)
 end
 
 # include deface overwrites
